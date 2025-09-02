5150a828318237a7ead2c4df859b3bba728b932a
oreorydz <rydzigor0232@gmail.com>
2025-09-02 12:05:47 +0200

    zespol uczniow

---
+++ src/ZespolUczniow.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class ZespolUczniow {
+    private ArrayList uczniowie;
+    private String nauczyciel;
+
+    public ZespolUczniow(String nauczyciel) {
+        ArrayList uczniowie = new ArrayList();
+        this.nauczyciel = nauczyciel;
+    }
+
+    public void dodajUczniaDoKlasy(){
+        uczniowie.add()
+    }
+}
